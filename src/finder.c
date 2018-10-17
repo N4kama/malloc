aff139dbc11f57d256dfd712acee22c0b1115992
romain.baguet <romain.baguet@epita.fr>
2018-10-17 18:28:22 +0200

    function find_p_meta : find page meta data (sized free list pointers)

--- src/finder.c
+++ src/finder.c
@@ -4,5 +4,9 @@ struct p_meta *find_p_meta(size_t size)
 {
     enum size s_enum = size_to_enum(size);
     struct p_meta *meta = get_head();
-    
+    while (meta->size != s_enum)
+    {
+	meta += 1;
+    }
+    return meta;
 }
